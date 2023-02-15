bd708ce4217835cb297e9f5ca1414d9805563b42
tOñO <antvhell@hotmail.com>
2023-02-15 16:51:05 -0600

    RestServer cascaron

--- models/server.js
+++ models/server.js
@@ -4,6 +4,7 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
+    this.usuariosPath = "/api/usuarios";
 
     // Middlewares
     this.middlewares();
@@ -16,40 +17,15 @@ class Server {
     // CORS
     this.app.use(cors());
 
+    // Lectura y parseo del body
+    this.app.use(express.json());
+
     // Directorio Publico
     this.app.use(express.static("public"));
   }
 
   routes() {
-    this.app.get("/api", (req, res) => {
-      res.json({
-        msg: "get API",
-      });
-    });
-
-    this.app.put("/api", (req, res) => {
-      res.json({
-        msg: "put API",
-      });
-    });
-
-    this.app.post("/api", (req, res) => {
-      res.json({
-        msg: "post API",
-      });
-    });
-
-    this.app.delete("/api", (req, res) => {
-      res.json({
-        msg: "delete API",
-      });
-    });
-
-    this.app.patch("/api", (req, res) => {
-      res.json({
-        msg: "patch API",
-      });
-    });
+    this.app.use(this.usuariosPath, require("../routes/usuarios"));
   }
 
   listen() {
